b891f8f5e74e50a18fcca2fa735045171f613d24
Greg Schwartz <greg@gregschwartz.net>
2024-10-08 19:42:02 -0700

    Add to Shopify

--- src/utils/shopifyUtils.js
+++ src/utils/shopifyUtils.js
@@ -1,14 +1,18 @@
 // src/utils/shopifyUtils.js
+const ERROR_MISSING_REQUIRED_FIELDS = 'Missing required field(s) for article';
+const ERROR_INVALID_URL = 'Error parsing URL';
 
 require('dotenv').config();
 const axios = require('axios');
 
-if (!process.env.SHOPIFY_CLIENT_KEY || !process.env.SHOPIFY_CLIENT_SECRET || !process.env.SHOPIFY_STORE_URL){// || !process.env.SHOPIFY_ACCESS_TOKEN) {
+if (!process.env.SHOPIFY_CLIENT_KEY || !process.env.SHOPIFY_CLIENT_SECRET || !process.env.SHOPIFY_STORE_URL || !process.env.SHOPIFY_ACCESS_TOKEN) {
   throw new Error(`Missing required environment variables for Shopify API: ${process.env.SHOPIFY_CLIENT_KEY}, ${process.env.SHOPIFY_CLIENT_SECRET}, ${process.env.SHOPIFY_STORE_URL}, ${process.env.SHOPIFY_ACCESS_TOKEN}`);
 }
 
+const SHOPIFY_API_VERSION = '2023-01';
+
 async function fetchShopifyProducts() {
-  const url = `https://${process.env.SHOPIFY_STORE_URL}/admin/api/2023-01/products.json`;
+  const url = `https://${process.env.SHOPIFY_STORE_URL}/admin/api/${SHOPIFY_API_VERSION}/products.json`;
 
   try {
     const response = await axios.get(url, {
@@ -18,7 +22,7 @@ async function fetchShopifyProducts() {
       }
     });
 
-    console.log('Products fetched successfully:', response.data.products);
+    // console.log('Products fetched successfully:', response.data.products);
     return response.data.products;
   } catch (error) {
     console.error('Error fetching products:', error.response ? error.response.data : error.message);
@@ -26,6 +30,75 @@ async function fetchShopifyProducts() {
   }
 }
 
+/**
+ * Adds the selected articles as products to Shopify.
+ * @param {Array} selectedArticles - The list of articles to add.
+ * @returns {Promise<Array>} The results of adding each article.
+ */
+async function addArticleToShopify(article) {
+  // Make sure it has title, description, at least one thumbnail, url
+  const { title, description, thumbnail, url } = article;
+  if (!title || !description || !thumbnail || !url) {
+    // console.error('Missing required field(s) for article:', article);
+    return ({ success: false, error: ERROR_MISSING_REQUIRED_FIELDS, article });
+  }
+
+  let hostname = '';
+  try {
+    hostname = new URL(url).hostname;
+  } catch (error) {
+    // console.error('Error parsing URL:', error);
+    return ({ success: false, error: ERROR_INVALID_URL, article });
+  }
+
+  try {
+    const productData = {
+      product: {
+        title: title,
+        body_html: description,
+        vendor: 'News Importer',
+        images: [{ src: thumbnail }],
+        tags: [
+          url
+        ],
+        options: [
+          { name: 'URL', values: [url] },
+          { name: 'Publication', values: [hostname] },
+        ],
+        variants: [
+          {
+            option1: url,
+            option2: hostname,
+          },
+        ],
+      },
+    };
+
+    const apiUrl = `https://${process.env.SHOPIFY_STORE_URL}/admin/api/${SHOPIFY_API_VERSION}/products.json`;
+    const response = await axios.post(apiUrl, productData, {
+      headers: {
+        'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (response.errors) {
+      throw new Error(JSON.stringify(response.errors));
+    }
+
+    const productId = response.data.product.id;
+    console.log(`${productId}: Added article: ${title}`); // Logging the added article
+    return ({ success: true, productId });
+  } catch (error) {
+    console.error(`Error adding article "${title}" to Shopify:`, error);
+    return ({ success: false, error: error.message, article });
+  }
+}
+
+
 module.exports = {
   fetchShopifyProducts,
+  addArticleToShopify,
+  ERROR_MISSING_REQUIRED_FIELDS,
+  ERROR_INVALID_URL,
 };
--- src/utils/shopifyUtils.test.js
+++ src/utils/shopifyUtils.test.js
@@ -1,15 +1,113 @@
 // src/utils/__tests__/shopifyUtils.test.js
 
-const { fetchShopifyProducts } = require('./shopifyUtils');
+const { fetchShopifyProducts, addArticleToShopify, ERROR_MISSING_REQUIRED_FIELDS, ERROR_INVALID_URL } = require('./shopifyUtils');
 
 describe('Shopify Utils', () => {
 
   describe('fetchShopifyProducts', () => {
     it('should fetch products successfully', async () => {
-      const products = await axiosFetch();
+      const products = await fetchShopifyProducts();
       expect(Array.isArray(products)).toBe(true);
-      console.log(products);
+      // console.log(products)
+
+      // products shouldn't be empty
+      expect(products.length).toBeGreaterThan(0);
     });
   });
+
+  describe('addArticleToShopify', () => {
+    const validArticle = {
+      title: 'Test Article 1',
+      url: 'http://example.com/test-url-1',
+      description: 'Description for Test Article 1',
+      thumbnail: 'http://example.com/test-url-1/image1.jpg'
+    };
+  
+    it('add article', async () => {
+      const results = await addArticleToShopify(validArticle);
+      // console.log(results)
+      expect(results).toMatchObject({
+        success: true,
+      });
+    });
+
+
+    // Helpers to create an article with missing fields
+    const requiredFields = ['title', 'url', 'description', 'thumbnail'];
+    const createArticleWithMissingFields = (fieldsToRemove) => {
+      const article = { ...validArticle };
+      fieldsToRemove.forEach(field => delete article[field]);
+      return article;
+    };
+  
+    // Test for each single missing field
+    requiredFields.forEach(field => {
+      it(`should return an error if ${field} is missing`, async () => {
+        const article = createArticleWithMissingFields([field]);
+        const results = await addArticleToShopify(article);
+        expect(results).toMatchObject({
+          success: false,
+          error: ERROR_MISSING_REQUIRED_FIELDS
+        });
+      });
+    });
+  
+    // Test for each combination of two missing fields
+    for (let i = 0; i < requiredFields.length; i++) {
+      for (let j = i + 1; j < requiredFields.length; j++) {
+        it(`should return an error if ${requiredFields[i]} and ${requiredFields[j]} are missing`, async () => {
+          const article = createArticleWithMissingFields([requiredFields[i], requiredFields[j]]);
+          const results = await addArticleToShopify(article);
+          expect(results).toMatchObject({
+            success: false,
+            error: ERROR_MISSING_REQUIRED_FIELDS
+          });
+        });
+      }
+    }
+  
+    // Test for each combination of three missing fields
+    for (let i = 0; i < requiredFields.length; i++) {
+      for (let j = i + 1; j < requiredFields.length; j++) {
+        for (let k = j + 1; k < requiredFields.length; k++) {
+          it(`should return an error if ${requiredFields[i]}, ${requiredFields[j]}, and ${requiredFields[k]} are missing`, async () => {
+            const article = createArticleWithMissingFields([requiredFields[i], requiredFields[j], requiredFields[k]]);
+            const results = await addArticleToShopify(article);
+            expect(results).toMatchObject({
+              success: false,
+              error: ERROR_MISSING_REQUIRED_FIELDS
+            });
+          });
+        }
+      }
+    }
+  
+    // Test for all fields missing
+    it('should return an error if all fields are missing', async () => {
+      const article = createArticleWithMissingFields(requiredFields);
+      const results = await addArticleToShopify(article);
+      expect(results).toMatchObject({
+        success: false,
+        error: ERROR_MISSING_REQUIRED_FIELDS
+      });
+    });
+  });
+  
+
+  // describe('removeExistingProducts', () => {
+  //   it('should correctly remove existing products from articles', async () => {
+  //     const articles = [
+  //       { title: 'Existing Product', url: 'existing-url', description: 'Desc', thumbnail: 'thumb1' },
+  //       { title: 'Unique New Product', url: 'unique-new-url', description: 'Desc', thumbnail: 'thumb2' },
+  //     ];
+
+  //     const result = await removeExistingProducts(articles);
+
+  //     expect(result).toEqual([
+  //       { title: 'Unique New Product', url: 'unique-new-url', description: 'Desc', thumbnail: 'thumb2' },
+  //     ]);
+  //   });
+  // });
+
 
 });
